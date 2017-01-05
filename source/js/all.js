e3ea4a8e38b71cf61e4eb278c31b4464a8f398e0
Koen Handekyn <koen.handekyn@up-nxt.com>
2017-01-06 00:26:08 +0100

    fixes and updates

--- source/js/all.js
+++ source/js/all.js
@@ -3,11 +3,14 @@
 //= require ie8
 //= require _jquery_mobile_touch_subset
 
-// function showalert(message,alerttype) {
-//   $('#alert_placeholder').append('<div id="alertdiv" class="alert ' +  alerttype + '"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
-//   setTimeout(function() {
-//     $("#alertdiv").remove();
-//   }, 10000);
-// }
+function showalert(message,alerttype) {
+  $('#alert_placeholder').append('<div id="alertdiv" class="alert ' +  alerttype + '"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
+  setTimeout(function() {
+    $("#alertdiv").remove();
+  }, 10000);
+}
 
-$('form[name=contact]').on('submit', on_submit);
+$(document).ready(function() {
+  $('form[name=contact]').on('submit', on_submit);
+});
+
