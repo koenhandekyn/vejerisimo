7c5ff8df3b6eea2ffe0661813bb3fdaf39bcf962
Koen Handekyn <koen.handekyn@up-nxt.com>
2017-01-07 16:54:51 +0100

    bundle update

---
+++ source/js/mobile.js
@@ -0,0 +1,16 @@
+//= require jquery/jquery.min
+//= require bootstrap/bootstrap
+//= require _jquery_mobile_touch_subset
+
+function showalert(message,alerttype) {
+  $('#alert_placeholder').append('<div id="alertdiv" class="alert ' +  alerttype + '"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
+  setTimeout(function() {
+    $("#alertdiv").remove();
+  }, 10000);
+}
+
+$(document).ready(function() {
+  $('form[name=contact]').on('submit', on_submit);
+  console.log('mobile form initialised');
+});
+
