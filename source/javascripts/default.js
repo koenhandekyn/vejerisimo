72bbcdc6c047397cc1f7a8b43af38b2e8b982c98
Koen Handekyn <koen.handekyn@up-nxt.com>
2017-01-09 09:07:12 +0100

    defered loading

--- source/javascripts/default.js
+++ source/javascripts/default.js
@@ -12,7 +12,28 @@ function showalert(message,alerttype) {
 
 var on_submit;
 
-$(document).ready(function() {
+$(function() {
   $('form[name=contact]').on('submit', on_submit);
   console.log('form initialised')
+});
+
+$(function() {
+  return $(".carousel.slide.lazy-img").on("slide.bs.carousel", function(ev) {
+    console.log("loading images");
+    var lazy;
+    lazy = $(ev.relatedTarget).find("img[data-src]");
+    lazy.attr("src", lazy.data('src'));
+    lazy.removeAttr("data-src");
+  });
+});
+
+$(function() {
+  return $(".carousel.slide.lazy-bg").on("slide.bs.carousel", function(ev) {
+    console.log("loading background images");
+    var lazy;
+    lazy = $(ev.relatedTarget).find(".img[data-src]");
+    var url = "url('"+lazy.data('src')+"')";
+    lazy.css("background-image", url);
+    lazy.removeAttr("data-src");
+  });
 });
